9bab596cf7000fb73addbf6d2fee3db544ad1733
Oleksandr Dychka <ol.dychka@gmail.com>
2026-05-08 09:26:27 -0400

    feat: sidebar

--- frontend/src/app/features/dashboard/dashboard/dashboard.ts
+++ frontend/src/app/features/dashboard/dashboard/dashboard.ts
@@ -6,7 +6,7 @@ import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [RouterLink],
+  imports: [],
   templateUrl: './dashboard.html',
 })
 export class Dashboard {}
--- frontend/src/app/features/dashboard/organization-layout/organization-layout.ts
+++ frontend/src/app/features/dashboard/organization-layout/organization-layout.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Sidebar } from '../sidebar/sidebar';
 
 @Component({
   selector: 'app-organization-layout',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Sidebar],
   templateUrl: './organization-layout.html',
 })
 export class OrganizationLayout {}
