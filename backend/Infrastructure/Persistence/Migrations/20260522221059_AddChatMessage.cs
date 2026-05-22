ef7b560b8ef7ee63111fc4175588960696e4f61e
Oleksandr Dychka <ol.dychka@gmail.com>
2026-05-22 18:15:38 -0400

    feat: ef core chat message migration

---
+++ backend/Infrastructure/Persistence/Migrations/20260522221059_AddChatMessage.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddChatMessage : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ChatMessages",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "character varying(36)", maxLength: 36, nullable: false),
+                    RoomId = table.Column<string>(type: "character varying(36)", maxLength: 36, nullable: false),
+                    SenderId = table.Column<string>(type: "character varying(36)", maxLength: 36, nullable: false),
+                    Content = table.Column<string>(type: "character varying(2000)", maxLength: 2000, nullable: false),
+                    SentAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ChatMessages", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ChatMessages_RoomId",
+                table: "ChatMessages",
+                column: "RoomId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ChatMessages");
+        }
+    }
+}
