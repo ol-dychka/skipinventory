376974bc772e441ab68b71dbdb7a13e615c054e4
Oleksandr Dychka <ol.dychka@gmail.com>
2026-06-10 07:50:52 -0400

    fix: chart styling, data appending

--- frontend/src/app/features/dashboard/dashboard/chart/chart.ts
+++ frontend/src/app/features/dashboard/dashboard/chart/chart.ts
@@ -10,21 +10,60 @@ export class ChartComponent implements AfterViewInit {
   @ViewChild('myChart') chartRef!: ElementRef;
   @Input() data!: SaleRecordSummaryLine[];
 
+  private getAppendedData(): SaleRecordSummaryLine[] {
+    const result = [
+      ...this.data.sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime()),
+    ];
+    const today = new Date();
+    today.setHours(0, 0, 0, 0);
+
+    const carrier =
+      result.length > 0
+        ? new Date(result[result.length - 1].date)
+        : new Date(today.getTime() - 9 * 24 * 60 * 60 * 1000);
+
+    carrier.setHours(0, 0, 0, 0);
+
+    while (carrier < today && result.length < 10) {
+      carrier.setDate(carrier.getDate() + 1);
+      result.push({
+        date: new Date(carrier),
+        total: 0,
+      });
+    }
+
+    return result;
+  }
+
+  private displayDate(date: Date) {
+    date = new Date(date);
+    return date.toISOString().split('T')[0];
+  }
+
   ngAfterViewInit() {
+    const appendedData = this.getAppendedData();
+
     new Chart(this.chartRef.nativeElement, {
       type: 'bar',
       data: {
-        labels: this.data.map((day) => day.date),
-        datasets: [{ label: 'Sales', data: this.data.map((day) => day.total) }],
+        labels: appendedData.map((day) => this.displayDate(day.date)),
+        datasets: [
+          {
+            label: 'Sales',
+            data: appendedData.map((day) => day.total),
+            backgroundColor: '#778da9',
+          },
+        ],
       },
       options: {
         maintainAspectRatio: false,
         responsive: true,
         scales: {
           y: {
-            min: Math.min(...this.data.map((day) => day.total)) - 10,
+            min: Math.min(...appendedData.map((day) => day.total)),
           },
         },
+        color: 'primary',
       },
     });
   }
