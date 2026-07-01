7a9d399d6a277533a1b3ac8f86d154ae90a8e88f
Oleksandr Dychka <ol.dychka@gmail.com>
2026-07-01 10:34:03 -0400

    feat: smart loading tips based on request processing time

--- frontend/src/app/core/services/loading-service.ts
+++ frontend/src/app/core/services/loading-service.ts
@@ -5,46 +5,60 @@ import { Injectable, signal } from '@angular/core';
 })
 export class LoadingService {
   private activeRequests = 0;
-  private timer: ReturnType<typeof setTimeout> | null = null;
+  private loadingTimer: ReturnType<typeof setTimeout> | null = null;
+  private wakingUpTimer: ReturnType<typeof setTimeout> | null = null;
 
   readonly isLoading = signal(false);
+  readonly isWakingUp = signal(false);
 
   show() {
+    // add count of request
     this.activeRequests++;
 
-    // already visible
-    if (this.isLoading()) {
+    // if previous request is still being processed, keep loading
+    if (this.isLoading() || this.loadingTimer) {
       return;
     }
 
-    // already waiting to become visible
-    if (this.timer) {
-      return;
-    }
-
-    this.timer = setTimeout(() => {
+    // show loading if request takes longer than set time (2s) to process
+    this.loadingTimer = setTimeout(() => {
       if (this.activeRequests > 0) {
         this.isLoading.set(true);
       }
 
-      this.timer = null;
-    }, 2500); // don't show for fast requests
+      this.loadingTimer = null;
+    }, 2000);
+
+    // show waking up if request takes longer than set time (10s) to process
+    this.wakingUpTimer = setTimeout(() => {
+      if (this.activeRequests > 0) {
+        this.isWakingUp.set(true);
+      }
+
+      this.wakingUpTimer = null;
+    }, 10000);
   }
 
   hide() {
+    // discard count of original request
     if (this.activeRequests > 0) {
       this.activeRequests--;
     }
 
+    // if that request wasn't the last one, keep loading
     if (this.activeRequests !== 0) {
       return;
     }
 
-    if (this.timer) {
-      clearTimeout(this.timer);
-      this.timer = null;
+    // if that request was the last one, stop loading
+    if (this.loadingTimer) {
+      clearTimeout(this.loadingTimer);
+      this.loadingTimer = null;
     }
-
+    if (this.wakingUpTimer) {
+      clearTimeout(this.wakingUpTimer);
+      this.wakingUpTimer = null;
+    }
     this.isLoading.set(false);
   }
 }
